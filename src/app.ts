c90112400a43c2b313a740a4f467744066d8e847
Hugo  Fortunato <hp.fortunato@gmail.com>
2026-02-05 12:23:26 -0300

    feat: add dependency inversion principle into code

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
-export const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    id: '1',
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-  },
-})
+app.register(appRoutes)
