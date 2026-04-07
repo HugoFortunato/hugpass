70fe887364db5b94320e54aa29a2ca527ff45065
Hugo  Fortunato <hp.fortunato@gmail.com>
2026-04-07 16:36:22 -0300

    feat: add profile controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,13 @@ import { register } from './controllers/register'
 import { membership } from './controllers/membership'
 import { authenticate } from './controllers/authenticate'
 import { fetchNearbyGyms } from './controllers/fetch-nearby-gyms'
+import { verifyJwt } from './middlewares/verify-jwt'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/membership', membership)
   app.post('/sessions', authenticate)
   app.get('/gyms/nearby', fetchNearbyGyms)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
