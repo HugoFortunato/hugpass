9e37ead702776a2054e7447f88ae27b90a7bde86
Hugo  Fortunato <hp.fortunato@gmail.com>
2026-07-09 13:30:00 -0300

    feat: create refresh token controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,12 +5,14 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { getActiveMembership } from './get-active-membership'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/membership', membership)
   app.post('/sessions', authenticate)
   app.get('/active-membership', getActiveMembership)
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
